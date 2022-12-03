42eb34667ba51b3b74a1154b10cdf0a50d192c92
Gudiya21 <gudia21@navgurukul.org>
2022-12-02 21:40:07 -0800

    I made table in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState } from 'react';
+function App(){
+    const values = [1,2,3,4,5,6,6,7,8,9,10]
+    const [table,setTable]=useState(0)
+    const [list,setList]=useState([])
+    const loopElement=()=>{
+      let newList=[]
+      for(let i=1;i<=10;i++){
+       newList.push(`${table} X ${i} = ${table*i}`)
+        console.log(newList)
+      }
+      setList(newList)
+      console.log(list)
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+    <div>
+      <input type="text" placeholder='enter a number' onChange={(e)=>{
+        setTable(e.target.value)
+      }}/>
+     <button onClick={loopElement}>Click</button>
+     
+      {
+        list.map((item,index)=>{
+          return(
+            <div >
+              <h4>{item}</h4>
+              
+            </div>
+          )
+        })
+      }
+
+      
+      
     </div>
-  );
+  )
 }
-
-export default App;
+export default App
